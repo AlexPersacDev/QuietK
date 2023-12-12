a377bf1397d68bd8b69caadfbb32e9d04d2faaa7
Alex Pereira Sacristan <alexpersac01@gmail.com>
2023-12-12 13:27:50 +0100

    Movement & jump
    Se pulió el movimiento del ujgador eliminando la aceleración infinita
    Se arregló la gravedad del proyecto  dejando pulido el salto

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,24 +4,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    //CharacterController playerController; 
     Rigidbody rbPlayer;
     [SerializeField] float speed; //velocidad de jugador
     [SerializeField] Transform cameraTransform; 
     Vector3 movDirection;
-    Vector3 yMovement;
     float h, v;
 
     [Header("Jump")]
-    [SerializeField] float gravityScale;
     [SerializeField] float jumpForce;
     [SerializeField] Transform playerFoot;
     [SerializeField] float footRad;
     [SerializeField] LayerMask whatIsGround;
+    int jumpsCount = 0;
 
     void Start()
     {
-        // playerController = GetComponent<CharacterController>();
         rbPlayer = GetComponent<Rigidbody>();
         Cursor.lockState = CursorLockMode.Locked;
     }
@@ -31,58 +28,44 @@ public class Player : MonoBehaviour
         h = Input.GetAxisRaw("Horizontal");
         v = Input.GetAxisRaw("Vertical");
         movDirection = cameraTransform.forward * v + cameraTransform.right * h;
-        movDirection.y = 0;
-        //GravityAplication();
-        PlayerMovement();
-
+        //movDirection.y = 0;
+        Jump();
+        CheckInGround();
     }
 
     private void FixedUpdate()
     {
-        Jump();
+        PlayerPhysicMovement();
     }
 
-    //void GravityAplication()
-    //{
-    //    yMovement.y += gravityScale * Time.deltaTime; //La gravedad se va incrementando cada frame.
-
-    //    playerController.Move(yMovement * Time.deltaTime); //Muevo al controller en el eje Y basandome en esa gravedad.
-    //}
-
-    void PlayerMovement()
-    {
-        rbPlayer.transform.Translate(movDirection.normalized * Time.deltaTime * speed);
-        //float xVelClamped = Mathf.Clamp(rbPlayer.velocity.x, 0, 5);
-        //float zVelClamped = Mathf.Clamp(rbPlayer.velocity.z, 0, 5);
-        //rbPlayer.AddForce(movDirection * speed, ForceMode.Force);
-        //if (v == 0 && h == 0)
-        //{
-        //    rbPlayer.velocity = new Vector3(0, rbPlayer.velocity.y, 0);
-        //}
-        //else if (rbPlayer.velocity.x > xVelClamped || rbPlayer.velocity.z > zVelClamped)
-        //{
-        //    if (rbPlayer.velocity.x > xVelClamped)
-        //    {
-        //        rbPlayer.velocity = new Vector3(xVelClamped, rbPlayer.velocity.y, rbPlayer.velocity.z);
-        //    }
-        //    else if (rbPlayer.velocity.z > zVelClamped)
-        //    {
-        //        rbPlayer.velocity = new Vector3(rbPlayer.velocity.x, rbPlayer.velocity.y, zVelClamped);
-        //    }
-        //}
-    }
-
-    void Jump()
+    void CheckInGround()
     {
         if (Physics.CheckSphere(playerFoot.position, footRad, whatIsGround))
         {
-           // yMovement.y = 0;
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                rbPlayer.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-                //float jump = Mathf.Sqrt(-2 * gravityScale * jumpHeigh);
-                //yMovement = new Vector3(0, jump, 0);
-            }
+            jumpsCount = 0;
+            rbPlayer.mass = 1;
+        }
+    }
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && jumpsCount < 2)
+        {
+            rbPlayer.velocity = Vector3.zero;
+            jumpsCount++;
+            rbPlayer.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    void PlayerPhysicMovement()
+    {
+        rbPlayer.AddForce(movDirection * speed, ForceMode.Force);
+        if (v == 0 && h == 0)
+        {
+            rbPlayer.velocity = new Vector3(0, rbPlayer.velocity.y, 0);
+        }
+        if (rbPlayer.velocity.magnitude > speed)
+        {
+            rbPlayer.velocity = Vector3.ClampMagnitude(rbPlayer.velocity, speed);
         }
     }
 }
